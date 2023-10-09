b4e7745102953bd9cfcc6da9fe10c88d9c699073
sa902 <7136810-sa902@users.noreply.replit.com>
2023-10-09 08:53:05 +0000

    added some styled code

--- src/Components/ResultsBox.jsx
+++ src/Components/ResultsBox.jsx
@@ -1,25 +1,46 @@
 import VisualCard from './VisualCard'
-export default function ResultsBox () {
+import styled from 'styled-components';
+
+const Container = styled.div`
+display:grid;
+		grid-gap: 16px;
+	@media (min-width: 768px){
+		grid-template-columns: repeat(2, 1fr);
+	}
+		@media (min-width: 960px){
+		grid-template-columns: repeat(3, 1fr);
+	}
+		@media (min-width: 1200px){
+		grid-template-columns: repeat(3, 1fr);
+	}
+`
+const Item = styled.div`
+		align-self:center;
+		align-content: center; 
+		justify-content: center;
+`
+
+export default function ResultsBox() {
 	return (
-		<div className="grid">
-			<div className="col">
+		<Container >
+			<Item>
 				<VisualCard />
-			</div>
-			<div className="col">
+			</Item>
+			<Item>
 				<VisualCard />
-			</div>
-			<div className="col">
+			</Item>
+			<Item>
 				<VisualCard />
-			</div>
-			<div className="col">
+			</Item>
+			<Item>
 				<VisualCard />
-			</div>
-			<div className="col">
+			</Item>
+			<Item>
 				<VisualCard />
-			</div>
-			<div className="col">
+			</Item>
+			<Item>
 				<VisualCard />
-			</div>
-		</div>
+			</Item>
+		</Container>
 	)
 }
