e78ff2c56f725626f892ae10ef5ad4766537e349
sam <samanderson902@gmail.com>
2023-10-11 18:04:06 +1300

    styiling changes

--- src/Components/CardTable.jsx
+++ src/Components/CardTable.jsx
@@ -1,20 +1,40 @@
-export default function CardTable({tableData}) {
-	return (
-		<table style={{ marginTop: "0.5em", marginBottom: "1em" }}>
-			<tbody>
-				<tr>
-					<th>Price</th>
-					<th>{tableData?.price}</th>
-				</tr>
-				<tr>
-					<td>Bedroom</td>
-					<td>{tableData?.bedroom}</td>
-				</tr>
-				<tr>
-					<td>Bathroom </td>
-					<td>{tableData?.bathroom}</td>
-				</tr>
-			</tbody>
-		</table>
-	)
-}
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledTable = styled.table`
+  margin: 0.5em 0;
+  font-family: 'inter', sans-serif; 
+  text-align: start;
+  width:60%;
+  tr{
+	display:flex;
+	justify-content:space-between;
+  }
+  th, td {
+	font-weight:bold;
+    font-size: 14px; 
+  }
+`;
+
+const CardTable = ({ tableData }) => {
+  return (
+    <StyledTable>
+      <tbody>
+        <tr>
+          <th>Price</th>
+          <th>{tableData?.price}</th>
+        </tr>
+        <tr>
+          <td>Bedroom</td>
+          <td>{tableData?.bedroom}</td>
+        </tr>
+        <tr>
+          <td>Bathroom</td>
+          <td>{tableData?.bathroom}</td>
+        </tr>
+      </tbody>
+    </StyledTable>
+  );
+};
+
+export default CardTable;
--- src/Components/VisualCard.jsx
+++ src/Components/VisualCard.jsx
@@ -4,7 +4,7 @@ export default function VisualCard({title, tableData,image}) {
 		<div style={{display: 'flex', justifyContent:'center'}}>
 			<img style={{ width: 325, height: 450, position: 'absolute' }} src="./CardBG.png" ></img>
 			
-			<div style={{ width: 325, height: 450, position: 'relative', flexDirection: 'column', justifyContent: 'space-evenly', alignItems: 'center', display: 'inline-flex' }}>
+			<div style={{ width: 325, height: 425, position: 'relative', flexDirection: 'column', justifyContent: 'space-evenly', alignItems: 'center', display: 'inline-flex' }}>
 
 				<div style={{ color: 'black', fontSize: 24, fontFamily: 'Inter', margin:15,padding:15, fontWeight: '400', wordWrap: 'break-word', paddingBottom: '0.5em' }}>{title}</div>
 
