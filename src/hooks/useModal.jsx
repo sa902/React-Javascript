c14ecdf30ee3f108a114144bf48c898773e6b2ed
sam <samanderson902@gmail.com>
2023-10-12 17:09:10 +1300

    fixed modal issue

--- src/hooks/useModal.jsx
+++ src/hooks/useModal.jsx
@@ -1,11 +1,24 @@
 import { useState } from "react";
+import styled, { createGlobalStyle } from 'styled-components';
 
+const GlobalStyle = createGlobalStyle`
+  body.body-no-scroll {
+    overflow: hidden;
+  }
+`;
 // Centralizes modal control
+
 const useModal = () => {
   const [modalOpen, setModalOpen] = useState(false);
 
-  const close = () => setModalOpen(false);
-  const open = () => setModalOpen(true);
+  const close = () => {
+    document.body.classList.remove('body-no-scroll');
+    setModalOpen(false)
+  };
+  const open = () =>{ 
+    document.body.classList.add('body-no-scroll');
+    setModalOpen(true)
+  };
 
   return { modalOpen, close, open };
 };
