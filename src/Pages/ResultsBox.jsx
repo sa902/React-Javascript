b5488cda942b889fe673375da606b2c57329261e
sam <samanderson902@gmail.com>
2023-10-12 18:52:07 +1300

    refactored resultsbox

--- src/Pages/ResultsBox.jsx
+++ src/Pages/ResultsBox.jsx
@@ -9,21 +9,11 @@ import Button from "../Components/Button";
 const Container = styled.div`
   display: grid;
   grid-gap: 16px;
-  @media (min-width: 768px) {
-    grid-template-columns: repeat(2, 1fr);
-  }
-  @media (min-width: 960px) {
-    grid-template-columns: repeat(3, 1fr);
-  }
-  @media (min-width: 1200px) {
-    grid-template-columns: repeat(3, 1fr);
-  }
+  grid-template-columns: repeat(auto-fill, minmax(251px, 1fr));
+  max-width: 1050px; /* Set a maximum width for the grid */
+  margin: 0 auto; /* Center align the grid */
 `;
-const Item = styled.div`
-  align-self: center;
-  align-content: center;
-  justify-content: center;
-`;
+
 const NewSearchButtonContainer = styled.div`
   display: flex;
   justify-content: center;
@@ -33,41 +23,21 @@ const NewSearchButtonContainer = styled.div`
 `;
 
 const fetchDataFromServer = (currentPage, searchData) => {
-  console.log(
-    `inside fetch data from server and current page here is ${currentPage}`,
-  );
-  const params = { ...{ currentPage: currentPage }, ...searchData };
-  return axios.get(
-    `https://sebhcug5f9.execute-api.eu-west-2.amazonaws.com/dev/Houses/GetAll`,
-    {
-      params,
-      paramsSerializer: (params) => {
-        return "reqParams=" + encodeURIComponent(JSON.stringify(params));
-      },
-    },
-  );
+  const params = { currentPage, ...searchData };
+  return axios.get(`https://sebhcug5f9.execute-api.eu-west-2.amazonaws.com/dev/Houses/GetAll`, {
+    params,
+    paramsSerializer: (params) => "reqParams=" + encodeURIComponent(JSON.stringify(params)),
+  });
 };
+
 export default function ResultsBox({ search, searchData }) {
-  let [currentPage, setCurrentPage] = useState(1);
-  let [totalPages, setTotalPages] = useState(0);
-  let { isLoading, data, isFetching } = useQuery(
-    ["get-houses", currentPage],
-    () => fetchDataFromServer(currentPage, searchData),
-    { enabled: true, refetchOnWindowFocus: false },
+  const [currentPage, setCurrentPage] = useState(1);
+  const { isLoading, data, isFetching } = useQuery(["get-houses", currentPage], () =>
+    fetchDataFromServer(currentPage, searchData),{ enabled: true, refetchOnWindowFocus: false }
   );
-  const setTotalPagesOnFirstSearch = () => {
-    data?.data?.pagination?.lastPage
-      ? setTotalPages(data?.data?.pagination?.lastPage)
-      : null;
-    console.log(
-      "inside  setTotalPagesOnFirstSearch",
-      data?.data?.pagination?.lastPage,
-    );
-  };
-  useEffect(setTotalPagesOnFirstSearch, [data]);
+  const totalPage = data?.data?.pagination?.lastPage;
   const onPageChange = (i) => {
     setCurrentPage(i);
-    console.log("Changing page, ", i);
   };
 
   if (isLoading || isFetching) {
@@ -80,22 +50,11 @@ export default function ResultsBox({ search, searchData }) {
         <Button clickFn={search} text={"Start New Search"} />
       </NewSearchButtonContainer>
       <Container>
-        {data?.data?.data.map((house) => {
-          return (
-            <Item key={house.id}>
-              <VisualCard
-                key={house.id}
-                house={house}
-              />
-            </Item>
-          );
-        })}
+        {data?.data?.data.map((house) => (
+          <VisualCard key={house.id} house={house} />
+        ))}
       </Container>
-      <Pagination
-        currentPage={currentPage}
-        totalPages={totalPages}
-        onPageChange={onPageChange}
-      ></Pagination>
+      <Pagination currentPage={currentPage} totalPages={totalPage} onPageChange={onPageChange} />
     </div>
   );
 }
