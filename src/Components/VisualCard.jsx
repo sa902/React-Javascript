d38e837936a2c56e7b4285d18169cf910dbcc47e
sam <samanderson902@gmail.com>
2023-10-12 20:30:53 +1300

    refactored the visual card

--- src/Components/VisualCard.jsx
+++ src/Components/VisualCard.jsx
@@ -1,91 +1,92 @@
+import React from "react";
 import CardTable from "./CardTable";
 import { AnimatePresence, motion } from "framer-motion";
-
 import useModal from "../hooks/useModal";
 import Modal from "./Modal";
-const handleCardClick = () => {
-  console.log("handled click");
-};
-export default function VisualCard({ house }) {
+import styled from "styled-components";
+
+const BackgroundImage = styled.img`
+width: 325px; 
+height: 450px;
+position: absolute;
+`
+const CardContent = styled.div`
+width: 325px;
+height: 425px;
+position: relative;
+flex-direction: column;
+justify-content: space-evenly;
+align-items: center;
+display: inline-flex;
+`
+
+const HouseImageContainer = styled.div`
+max-width: 192px;
+max-height: 233px;
+border-radius: 5;
+margin-right: 1em;
+`
+const TitleContainer = styled.div`
+color: black;
+font-size: 30px;
+margin: 15px;
+padding: 15px;
+font-weight: 400;
+word-wrap: break-word;
+padding-bottom: 0.5em;
+`
+
+const VisualCard = ({ house }) => {
   const { modalOpen, close, open } = useModal();
   const { title, image_ids, bedrooms, bathrooms, price } = house;
+
   return (
     <>
-      <motion.main>
-        <motion.div
-          whileHover={{ scale: 1.1 }}
-          whileTap={{ scale: 0.9 }}
-          className="save-button"
-          onClick={open}
-        >
-          <div
-            style={{ display: "flex", justifyContent: "center" }}
-            onClick={handleCardClick}
-          >
-            <img
-              style={{ width: 325, height: 450, position: "absolute" }}
-              src="./CardBG.png"
-            ></img>
-
-            <div
-              style={{
-                width: 325,
-                height: 425,
-                position: "relative",
-                flexDirection: "column",
-                justifyContent: "space-evenly",
-                alignItems: "center",
-                display: "inline-flex",
-              }}
-            >
-              <div
-                style={{
-                  color: "black",
-                  fontSize: 30,
-                  fontFamily: "Inter",
-                  margin: 15,
-                  padding: 15,
-                  fontWeight: "400",
-                  wordWrap: "break-word",
-                  paddingBottom: "0.5em",
-                  fontFamily: "Patrick Hand",
-                }}
-              >
-                {title}
-              </div>
-
-              <div
-                style={{
-                  maxWidth: 192,
-                  maxHeight: 233,
-                  background: "",
-                  borderRadius: 5,
-                  marginRight: "1em",
-                }}
-              >
-                <img
-                  src={`https://trademe.tmcdn.co.nz/photoserver/med/${image_ids[0]}.jpg`}
-                ></img>
-              </div>
-              <CardTable
-                bedrooms={bedrooms}
-                bathrooms={bathrooms}
-                price={price}
-              ></CardTable>
-            </div>
-          </div>
-        </motion.div>
-      </motion.main>
-      <AnimatePresence initial={false} mode={"wait"}>
-        {modalOpen && (
-          <Modal
-            modalOpen={modalOpen}
-            text={title}
-            house={house}
-            handleClose={close}
+    <motion.main>
+      <motion.div
+        whileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 0.9 }}
+        className=""
+        onClick={open}
+      >
+        <div class="centre">
+          <BackgroundImage
+            src="./CardBG.png"
+            alt="Card Background"
           />
-        )}
-      </AnimatePresence>
+          <CardContent>
+            <TitleContainer>
+              {title}
+            </TitleContainer>
+
+            <HouseImageContainer>
+              <img
+                src={`https://trademe.tmcdn.co.nz/photoserver/med/${image_ids[0]}.jpg`}
+                alt="House Image"
+              />
+            </HouseImageContainer>
+            <CardTable
+              bedrooms={bedrooms}
+              bathrooms={bathrooms}
+              price={price}
+            />
+          </CardContent>
+        </div>
+      </motion.div>
+    </motion.main>
+
+    <AnimatePresence initial={false} mode={"wait"}>
+      {modalOpen && (
+        <Modal
+          modalOpen={modalOpen}
+          text={title}
+          house={house}
+          handleClose={close}
+        />
+      )}
+    </AnimatePresence>
     </>
   );
-}
+};
+
+export default VisualCard;
